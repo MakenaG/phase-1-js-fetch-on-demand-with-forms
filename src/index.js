bbc112cc47b8257d3134ff3900e7ceadcf946f6f
MakenaG <makenaglory44@gmail.com>
2022-12-04 23:47:23 +0300

    commit tree 730beac770f96c4e839f2ccf99cdca08935a30b5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 const init = () => {
+    const form = document.querySelector("form");
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    form.addEventListener("submit", getMovie)
+  };
+  
+  // render movie details
+  function renderMovie(movie) {
+    const movieDetails = document.querySelector("#movieDetails");
+    const header = movieDetails.querySelector("h4");
+    const summary = movieDetails.querySelector("p");
+    header.innerHTML = movie.title;
+    summary.innerHTML = movie.summary;
+  }
+  
+  
+  // get movie data
+  function getMovie(e) {
+    e.preventDefault();
+    const id = document.querySelector("#searchByID").value;
+    if(id > 0 && id <= 3){
+    fetch(`http://localhost:3000/movies/${id}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        renderMovie(data);
+      });
+    }else{
+      alert("id not found")
+      location.reload()
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", init);
